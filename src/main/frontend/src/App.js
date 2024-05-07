e671f07c45a5fe101b2518005c87b0327d21aee6
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-05-07 16:57:24 +0900

    Feat: 추가 APMain UI

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import MainPage from './pages/MainPage';
+import APMain from './pages/AP/APMain';
 
 function App() {
   return (
     <div className='App'>
       <Routes>
         <Route path='/' element={<MainPage />}></Route>
+        <Route path='/ai-planner' element={<APMain />}></Route>
       </Routes>
     </div>
   );
