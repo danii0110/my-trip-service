d9d508c101b475c321794473c733f6176aaa3411
KimSeoYeon0130 <ssbsoul@skuniv.ac.kr>
2024-05-20 06:29:54 +0900

    Feat: 추가 Router path

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,12 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import MainPage from './pages/MainPage';
+import TripPlanPage from "./pages/TripPlan/TripPlanPage";
+import AIChatPage from "./pages/AIChat/AIChatPage";
+import LocationPage from "./pages/TripPlan/LocationPage";
+import SearchPage from "./pages/Search/SearchPage";
 
 function App() {
   return (
     <div className='App'>
       <Routes>
         <Route path='/' element={<MainPage />}></Route>
+        <Route path='/trip-plan' element={<TripPlanPage />}></Route>
+        <Route path='/search' element={<SearchPage />}></Route>
+        <Route path='/ai-chat' element={<AIChatPage />}></Route>
+        <Route path='/location' element={<LocationPage />}></Route>
       </Routes>
     </div>
   );
