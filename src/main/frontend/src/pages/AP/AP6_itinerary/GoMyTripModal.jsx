693f51ebff8d6e37b76570587e23c20392d59be2
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-05-23 11:22:25 +0900

    Feat: 추가 AP6 모달창

---
+++ src/main/frontend/src/pages/AP/AP6_itinerary/GoMyTripModal.jsx
@@ -0,0 +1,37 @@
+import styles from './GoMyTripModal.module.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
+const GoMyTripModal = ({ show, onHide }) => {
+  const navigate = useNavigate();
+  const cancleonClick = () => {
+    onHide();
+  };
+
+  const goToMyTrip = () => {
+    onHide();
+    navigate('/my-info');
+  };
+  return (
+    <div className={styles.container}>
+      <Modal show={show} onHide={onHide} size='lg' aria-labelledby='contained-modal-title-vcenter' centered>
+        <Modal.Body className={styles.customModalBody}>
+          <div className={styles.content}>
+            <p>저장되었습니다.</p>
+            <p>마이페이지로 이동하시겠습니까?</p>
+          </div>
+          <div className={styles.btnCont}>
+            <button className={styles.cancleBtn} type='button' onClick={cancleonClick}>
+              닫기
+            </button>
+            <button className={styles.okBtn} type='button' onClick={goToMyTrip}>
+              확인
+            </button>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+export default GoMyTripModal;
