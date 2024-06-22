4aec633ca088ceb2c2575685027e8de4129998c6
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-06-22 18:34:30 +0900

    Feat: AP5 선택한 장소

--- src/main/frontend/src/pages/AP/AP5_planList/PlaceCont.jsx
+++ src/main/frontend/src/pages/AP/AP5_planList/PlaceCont.jsx
@@ -1,9 +1,11 @@
 import styles from './PlaceCont.module.scss';
 
-const PlaceCont = ({ placeName }) => {
+const PlaceCont = ({ placeName, placeImage }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.imgCont}></div>
+      <div className={styles.imgCont}>
+        {placeImage ? <img src={placeImage} alt={placeName} className={styles.image} /> : null}
+      </div>
       <div className={styles.placeName}>{placeName}</div>
     </div>
   );
